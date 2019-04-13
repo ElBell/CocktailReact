b3eaf52d4d7d86bd8fd5578e016dc2f529e44278
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-12 22:33:58 -0400

    changed back to fetch

--- src/components/SearchPages/SearchIngredient.js
+++ src/components/SearchPages/SearchIngredient.js
@@ -3,7 +3,6 @@ import {ListPage} from "../DrinkList/ListPage";
 import {IngredientPage} from "../IngredientList/IngredientPage";
 import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 import {BackButton} from "../Utils/BackButton";
-import axios from "axios";
 
 
 export class SearchIngredient extends React.Component{
@@ -15,11 +14,13 @@ export class SearchIngredient extends React.Component{
 
   getDrinks() {
     if (this.state.limit) {
-      axios.get( "https://cocktail-compendium-spring.herokuapp.com/cocktail/ingredients/limit" + this.state.ingredients)
-        .then(({data}) => {this.setState({drinks: data})});
+      fetch("https://cocktail-compendium-spring.herokuapp.com/cocktail/ingredients/limit/" + this.state.ingredients)
+        .then(response => response.json())
+        .then(data => this.setState({drinks: data}))
     } else {
-      axios.get( "https://cocktail-compendium-spring.herokuapp.com/cocktail/ingredients/include" + this.state.ingredients)
-        .then(({data}) => {this.setState({drinks: data})});
+      fetch("https://cocktail-compendium-spring.herokuapp.com/cocktail/ingredients/include/" + this.state.ingredients)
+        .then(response => response.json())
+        .then(data => this.setState({drinks: data}))
     }
   }
 
